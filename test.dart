5d32780cf91d1120c956da42d786bcab95bff1fa
Joao Pedro <laharlx@hotmail.com>
2020-01-17 20:18:07 -0300

    Testes do modelo

--- test.dart
+++ test.dart
@@ -5,6 +5,26 @@ void main(){
   test("Model Testing", (){
     Pokemon pokemon = Pokemon(nome: 'Pikachu', pokedexIndex: 1, abilities: ['test','test']);
     expect(pokemon.nome, equals('Pikachu'));
-  });
-
+    Map<String, dynamic> mockJson={ 
+      "forms" : [
+        { 
+          "name": "pikachu"
+        }
+      ],
+      "game_indices": [
+        {
+          "game_index": 1
+        }
+      ],
+      "abilities": [
+        "test", "test"
+      ]
+    };
+    
+  
+    Pokemon otherPokemon = Pokemon.fromJson (mockJson);
+    expect(otherPokemon.nome, equals('pikachu'));
+    expect(otherPokemon.pokedexIndex,equals(1));
+    expect(otherPokemon.abilities, equals(["test","test"]));
+    });
 }
